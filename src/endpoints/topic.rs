eb64b084ac3e63b4c819c3727ef73e80cdb22031
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-05-15 22:54:08 +0100

    Remove old commented out code

--- src/endpoints/topic.rs
+++ src/endpoints/topic.rs
@@ -1,12 +1,11 @@
 use actix_web::{get, web, HttpRequest, HttpResponse, Responder};
-use base64::{engine, Engine};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use sqlx::PgPool;
 use std::rc::Rc;
 use tokio::try_join;
 
-use crate::{badRequest, internalServerError, unauthorized, AppState};
+use crate::{badRequest, internalServerError, AppState};
 
 #[derive(Deserialize)]
 struct ArgumentsRequest {
@@ -37,18 +36,6 @@ async fn topic_endpoint(
 	title_req: web::Query<ArgumentsRequest>,
 	app_state: web::Data<AppState>,
 ) -> impl Responder {
-	// let auth = match Authorization::<Basic>::parse(&req) {
-	// 	Ok(res) => res,
-	// 	Err(e) => return unauthorized!("Authorization header error: {e}"),
-	// };
-	// let email = auth.as_ref().user_id();
-	// let token = auth.as_ref().password();
-	// let authorization_result = check_authorization(email, token, &app_state.dbpool).await;
-	// let is_authorized = unwrap_or_esalate!(authorization_result);
-	// if !is_authorized {
-	// 	return unauthorized!("Incorrect username or key");
-	// }
-	// let title = &title_req.title;
 	let id = 1;
 	let body = match get_body(id, &app_state.dbpool).await {
 		Ok(res) => res,
@@ -83,30 +70,6 @@ async fn get_body(id: i64, dbpool: &PgPool) -> Result<String, HttpResponse> {
 		},
 	});
 	Ok(body.to_string())
-}
-
-/// Returns `Ok(true)` if authorized, `Ok(false)` if unauthorized and `Err(_)` on error
-async fn check_authorization(
-	email: &str,
-	session_token: Option<&str>,
-	db_pool: &PgPool,
-) -> Result<bool, HttpResponse> {
-	let token = session_token.ok_or(unauthorized!("No session token provided"))?;
-	let base64_decoder = engine::general_purpose::URL_SAFE;
-	let token_bytes = base64_decoder
-		.decode(token)
-		.or_else(|e| Err(unauthorized!("Session token decode error: {e}")))?;
-	let result = sqlx::query!(
-		r#"SELECT COUNT(1) AS "count!" FROM session_tokens WHERE email=$1 AND token=$2;"#,
-		email,
-		token_bytes
-	)
-	.fetch_one(db_pool)
-	.await;
-	match result {
-		Ok(res) => Ok(res.count >= 1),
-		Err(e) => Err(internalServerError!("Error retrieving user data: {e}")),
-	}
 }
 
 async fn get_topic_arguments(topic_id: i64, db_pool: &PgPool) -> Result<Topic, HttpResponse> {
