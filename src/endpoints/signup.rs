a85e4e71f6a91c971a1ac416d7b614bfd393ea7e
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-07-29 12:54:36 +0100

    Refactor email verification

--- src/endpoints/signup.rs
+++ src/endpoints/signup.rs
@@ -1,13 +1,13 @@
 use crate::email_helper::send_email;
 use crate::hashing_helper::hash_string;
-use crate::AppState;
 use crate::{badRequest, internalServerError};
+use crate::{unwrap_or_esalate, AppState};
 use actix_web::{post, web, HttpResponse, Responder};
-use log::warn;
+use log::{error, warn};
 use rand::distributions::{Alphanumeric, DistString};
 use serde::Deserialize;
-use serde_json::json;
-use sqlx::postgres::PgQueryResult;
+use sqlx::error::DatabaseError;
+use sqlx::PgPool;
 
 #[derive(Deserialize)]
 struct SignupRequest {
@@ -25,58 +25,76 @@ async fn signup_endpoint(
 		Ok(res) => res,
 		Err(e) => return e,
 	};
-	// Generate a random token with `thread_rng` which is cryptographically secure
-	let verification_token = Alphanumeric.sample_string(&mut rand::thread_rng(), 6);
 	let result = sqlx::query!(
-		"INSERT INTO users(email, username, password_hash, verification_token) VALUES ($1, $2, $3, $4);",
+		"INSERT INTO users(email, username, password_hash) VALUES ($1, $2, $3) RETURNING id",
 		request.email,
 		request.username,
 		password_hash.as_str(),
+	)
+	.fetch_one(&app_state.dbpool)
+	.await;
+
+	let res = match result {
+		Ok(res) => res,
+		Err(sqlx::Error::Database(db_error)) => return check_errors(db_error),
+		Err(e) => {
+			warn!("Error creating account: {e}");
+			return internalServerError!("Error creating account");
+		}
+	};
+	let email_result = send_verification_email(res.id, &request.email, &app_state.dbpool).await;
+	unwrap_or_esalate!(email_result);
+	HttpResponse::Ok().finish()
+}
+
+async fn send_verification_email(
+	id: i64,
+	email: &str,
+	dbpool: &PgPool,
+) -> Result<(), HttpResponse> {
+	// Generate a random token with `thread_rng` which is cryptographically secure
+	let verification_token = Alphanumeric.sample_string(&mut rand::thread_rng(), 6);
+
+	let result = sqlx::query!(
+		"INSERT INTO email_verification_tokens(id, token) VALUES ($1, $2)",
+		id,
 		verification_token,
 	)
-	.execute(&app_state.dbpool)
+	.execute(dbpool)
 	.await;
-	send_email(
-		&request.email,
+
+	match result {
+		Ok(_) => {} //TODO check number of rows affected
+		Err(e) => {
+			error!("Error saving verification token: {e}");
+			return Err(internalServerError!("Error verifying email"));
+		}
+	};
+
+	let success = send_email(
+		email,
 		"Confirm account",
 		format!("Your verification token is: {verification_token}"),
 	);
-	check_errors(result)
-}
-
-fn check_errors(result: Result<PgQueryResult, sqlx::Error>) -> HttpResponse {
-	match result {
-		Ok(res) => success(res),
-		Err(sqlx::Error::Database(db_error)) => {
-			if db_error.is_unique_violation() {
-				match db_error.constraint() {
-					Some("users_email_key") => {
-						return badRequest!("An account with that email already exists")
-					}
-					Some("users_username_key") => {
-						return badRequest!("An account with that username already exists")
-					}
-					_ => {}
-				};
-			};
-			warn!("Database error on creating account: {}", db_error.message());
-			internalServerError!("Error creating account")
-		}
-		Err(e) => {
-			warn!("Error creating account: {e}");
-			internalServerError!("Error creating account")
-		}
+	if success {
+		Ok(())
+	} else {
+		Err(badRequest!("Cannot send email to address: {email}"))
 	}
 }
 
-fn success(res: PgQueryResult) -> HttpResponse {
-	let body = json!({
-		"token": 0
-	});
-	let rows = res.rows_affected();
-	if rows != 1 {
-		warn!("Unexpected number of rows affected: {rows}");
-		// Return success to user but log unexpected rows affected
+fn check_errors(db_error: Box<dyn DatabaseError>) -> HttpResponse {
+	if db_error.is_unique_violation() {
+		match db_error.constraint() {
+			Some("users_email_key") => {
+				return badRequest!("An account with that email already exists")
+			}
+			Some("users_username_key") => {
+				return badRequest!("An account with that username already exists")
+			}
+			_ => {}
+		};
 	};
-	HttpResponse::Ok().body(body.to_string())
+	warn!("Database error on creating account: {}", db_error.message());
+	internalServerError!("Error creating account")
 }
--- src/endpoints/verify_email.rs
+++ src/endpoints/verify_email.rs
@@ -1,5 +1,5 @@
-use actix_web::{post, web, HttpResponse, Responder};
-use log::warn;
+use actix_web::{post, web, HttpRequest, HttpResponse, Responder};
+use log::{info, warn};
 use serde::Deserialize;
 use sqlx::PgPool;
 
@@ -13,29 +13,49 @@ struct VerifyEmailRequest {
 
 #[post("/verifyemail")]
 async fn verify_email_endpoint(
+	req: HttpRequest,
 	form: web::Json<VerifyEmailRequest>,
 	app_state: web::Data<AppState>,
 ) -> impl Responder {
 	let token = &form.token;
-	match is_correct_token(token, &app_state.dbpool).await {
-		Ok(true) => HttpResponse::Ok().finish(),
-		Ok(false) => unauthorized!("Incorrect or expired token"),
+	match is_correct_token(token, &app_state.dbpool, &req).await {
+		Ok(()) => HttpResponse::Ok().finish(),
 		Err(e) => e,
 	}
 }
 
-pub async fn is_correct_token(token: &str, db_pool: &PgPool) -> Result<bool, HttpResponse> {
+pub async fn is_correct_token(
+	token: &str,
+	db_pool: &PgPool,
+	req: &HttpRequest,
+) -> Result<(), HttpResponse> {
 	let result = sqlx::query!(
-		"SELECT EXISTS(SELECT 1 FROM users WHERE verification_token=$1);",
+		"DELETE FROM email_verification_tokens WHERE token = $1 RETURNING id",
 		token,
 	)
 	.fetch_one(db_pool)
 	.await;
+	let id = match result {
+		Ok(res) => Ok(res.id),
+		//TODO add more fine-graned error checking
+		Err(_) => {
+			match req.peer_addr() {
+				Some(ip) => info!("Failed email verification attempt from ip={ip}"),
+				None => warn!("Failed email verification attempt from unknown ip"),
+			};
+			Err(unauthorized!("Incorrect or expired token"))
+		}
+	}?;
+	let result = sqlx::query!("UPDATE users SET email_verified = true WHERE id = $1", id)
+		.execute(db_pool)
+		.await;
 	match result {
-		Ok(res) => Ok(res.exists.unwrap_or(false)),
+		Ok(_) => Ok(()), //TODO check number of rows affected
 		Err(e) => {
-			warn!("Error checking if token is correct(token={token}): {e}");
-			Err(internalServerError!("Unknown error 3"))
+			warn!("Error updating user to verified (id={id}, token={token}): {e}");
+			Err(internalServerError!(
+				"Unable to verify email (don't worry, we're working on it)"
+			))
 		}
 	}
 }
