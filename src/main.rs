0e9f03ab84ddeefe6e7e0269a3e39856398b7f40
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-08-05 22:59:23 +0100

    Get port from arguments and CORS origin from env

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+use std::env;
+
 use actix_cors::Cors;
 use actix_web::{http, web, App, HttpServer};
 use eris::*;
+use general_helper::get_env;
 use log::info;
 
-const PORT: u16 = 9000;
+const DEFAULT_PORT: u16 = 9000;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 	env_logger::init();
+	let port = get_port();
 	let app_state = database::init_app_state().await;
 	let server = HttpServer::new(move || {
 		App::new()
@@ -24,15 +28,27 @@ async fn main() -> std::io::Result<()> {
 			.service(topics::topics_endpoint)
 			.service(verify_email::verify_email_endpoint)
 	})
-	.bind(("0.0.0.0", PORT))?
+	.bind(("0.0.0.0", port))?
 	.run();
-	info!("Server initialised on port {PORT}!");
+	info!("Server initialised on port {port}!");
 	server.await
 }
 
+fn get_port() -> u16 {
+	let mut args = env::args();
+	args.next(); // Get rid of binary name
+	if args.next() == Some("--port".to_owned()) {
+		let string_arg = args.next().expect("Must specify port number after --port");
+		string_arg.parse().expect("Port number must be a string")
+	} else {
+		DEFAULT_PORT
+	}
+}
+
 fn get_cors() -> Cors {
+	let origin = get_env("FRONTEND_URL");
 	Cors::default()
-		.allowed_origin("http://localhost:3000")
+		.allowed_origin(&origin)
 		.allowed_header(http::header::CONTENT_TYPE)
 		.supports_credentials()
 		.allowed_methods(["GET", "POST", "PUT", "DELETE"])
